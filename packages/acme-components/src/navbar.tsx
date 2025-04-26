2e5eaeaf8ef932973deba249d9dde031d89f8277
cepish <cepish13@gmail.com>
2025-04-26 14:23:56 +0300

    components: navbar text upd

--- packages/acme-components/src/navbar.tsx
+++ packages/acme-components/src/navbar.tsx
@@ -4,25 +4,25 @@ export function Navbar({ isDocsApp }: { isDocsApp?: boolean }) {
   return isDocsApp ? (
     <ul className="inline-flex mb-4">
       <li>
-        <A href="/">Page: Home (Multi-Zones)</A>
+        <A href="/">Home (Multi-Zones)</A>
       </li>
       <li className="ml-4">
-        <Link href="/docs">Page: Docs</Link>
+        <Link href="/docs">Docs</Link>
       </li>
       <li className="ml-4">
-        <Link href="/docs/about">Page: About Docs</Link>
+        <Link href="/docs/about">About Docs</Link>
       </li>
     </ul>
   ) : (
     <ul className="inline-flex mb-4">
       <li>
-        <Link href="/">Page: Home</Link>
+        <Link href="/">Home</Link>
       </li>
       <li className="ml-4">
-        <Link href="/about">Page: About</Link>
+        <Link href="/about">About</Link>
       </li>
       <li className="ml-4">
-        <A href="/docs">Page: Docs (Multi-Zones)</A>
+        <A href="/docs">Docs (Multi-Zones)</A>
       </li>
     </ul>
   )
