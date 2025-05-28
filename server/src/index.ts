bac6a5f8443dd1e27e87ab967283cfd9c6654ad9
cepish <cepish13@gmail.com>
2025-05-28 23:46:44 +0300

    server dotenv added

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,18 +1,25 @@
 import express, { Request, Response } from "express";
+import * as dotenv from "dotenv";
 import entitiesRouter from "./routes/entities";
 
+dotenv.config();
 const app = express();
-const PORT = 9000;
+
+const { NODE_ENV, PORT = 9000 } = process.env;
+const isDev = NODE_ENV === "development";
 
 app.use(express.json());
 app.use("/entities", entitiesRouter);
 
 // Root route
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+  res.send("Render server is running");
 });
 
 // Start the server
 app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
+  console.log(`Listening to port ${PORT} in ${NODE_ENV} mode`);
+  if (isDev) {
+    console.log(`Open http://localhost:${PORT}/ and start hacking!`);
+  }
 });
