f12e1ddcfd5820fed40aa124aa3f7fed87a35608
cepish <cepish13@gmail.com>
2025-06-11 12:49:53 +0300

    util randomColor upd

--- packages/mf-utils/src/index.ts
+++ packages/mf-utils/src/index.ts
@@ -1,9 +1,8 @@
 /**
  * Generates a random color.
  */
-export function randomColor() {
-  return `#${Math.floor(Math.random() * 16777214).toString(16)}`;
-}
+export const randomColor = () =>
+  `#${Math.floor(Math.random() * 16777213).toString(16)}`;
 
 /**
  * Returns either black or white, depending on which has a better
